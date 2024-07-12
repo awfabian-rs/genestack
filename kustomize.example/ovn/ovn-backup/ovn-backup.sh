3c5cdb852569df991155879d02626794b2f1ae3b
Adam Fabian <adam.fabian@rackspace.com>
2024-07-12 16:55:54 -0500

    WIP OSPC-554 add metric gathering to ovn-backup.sh
    This starts changing the ovn-backup.sh script to gather metrics for Prometheus.
    
    The metrics also look useful outside of Prometheus.
    
    I will still need to add pushing the metrics to the push gateway. I think I may
    need to use some kind of trap in the script to make sure it doesn't exit without
    pushing the metrics.

--- kustomize.example/ovn/ovn-backup/ovn-backup.sh
+++ kustomize.example/ovn/ovn-backup/ovn-backup.sh
@@ -28,7 +28,6 @@ log_level() {
         ;;
     esac
 }
-export -f log_level
 
 log_line() {
     local LEVEL
@@ -40,7 +39,37 @@ log_line() {
         echo "$line" | tee -a "$LOG_FILE"
     fi
 }
-export -f log_line # exported for upload_file
+
+# Stats files init. These mostly get used to send to Prometheus, but you could
+# just read them if you want to.
+
+STATS_DIR="{$BACKUP_DIR}/stats"
+
+[[ -d "$STATS_DIR" ]] || mkdir "$STATS_DIR"
+
+metrics=("run_count" "save_to_disk_success_count" "save_to_disk_failure_count"
+"upload_attempt_count" "upload_success_count" "upload_failure_count"
+"disk_files_gauge" "swift_objects_gauge")
+
+for metric_filename in "${metrics[@]}"
+do
+    metric_file_fullname="${STATS_DIR}/$metric_filename"
+    [[ -e "$metric_file_fullname" ]] || echo "0" > "$metric_file_fullname"
+done
+
+# end stats file init
+
+# Function to increment a stats counter $1
+increment() {
+    local STAT_NAME
+    STAT_NAME="$1"
+    STAT_FULL_FILENAME="${STATS_DIR}/$STAT_NAME"
+    VALUE="$(cat $STAT_FULL_FILENAME)"
+    ((VALUE++))
+    echo "$VALUE" > $STAT_FULL_FILENAME
+}
+
+increment run_count
 
 # Delete old backup files on volume.
 cd "$BACKUP_DIR" || exit 2
@@ -51,8 +80,24 @@ find "$BACKUP_DIR" -ctime +"$RETENTION_DAYS" -delete;
 YMD="$(date +"%Y/%m/%d")"
 # kubectl-ko creates backups in $PWD, so we cd first.
 mkdir -p "$YMD" && cd "$YMD" || exit 2
-/kube-ovn/kubectl-ko nb backup || log_line ERROR "nb backup failed"
-/kube-ovn/kubectl-ko sb backup || log_line ERROR "sb backup failed"
+FAILED=false
+
+if ! /kube-ovn/kubectl-ko nb backup
+then
+    log_line ERROR "nb backup failed"
+    FAILED=true
+fi
+if ! /kube-ovn/kubectl-ko sb backup
+then
+    log_line ERROR "sb backup failed"
+    FAILED=true
+fi
+if [[ "$FAILED" == "true" ]]
+then
+    increment save_to_disk_failure_count
+else
+    increment save_to_disk_success_count
+fi
 
 if [[ "$SWIFT_TEMPAUTH_UPLOAD" != "true" ]]
 then
@@ -62,6 +107,8 @@ fi
 # Everything from here forward deals with uploading to a Swift with tempauth.
 
 cd "$BACKUP_DIR" || exit 2
+
+increment upload_attempt_count
 
 # Make a working "swift" command
 SWIFT="kubectl -n openstack exec -i openstack-admin-client --
@@ -87,13 +134,26 @@ upload_file() {
       log_line INFO "SUCCESSFUL UPLOAD $FILE as object $OBJECT_NAME"
     else
       log_line ERROR "FAILURE API swift exited $? uploading $FILE as $OBJECT_NAME"
+      FAILED_UPLOAD=true
     fi
 }
 export -f upload_file
 
 # find created backups and upload them
 cd "$BACKUP_DIR" || exit 2
-# unusual find syntax to use an exported function from the shell
-find "$YMD" -type f -newer "$BACKUP_DIR/last_upload" \
--exec bash -c 'upload_file "$0"' {} \;
+
+FAILED_UPLOAD=false
+find "$YMD" -type f -newer "$BACKUP_DIR/last_upload" | \
+while read file
+do
+    upload_file "$file"
+done
+
+if [[ "$FAILED_UPLOAD" == "true" ]]
+then
+    increment upload_failure_count
+else
+    increment upload_success_count
+fi
+
 touch "$BACKUP_DIR/last_upload"
