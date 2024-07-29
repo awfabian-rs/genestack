10a22ef87adc30e2b16010f62477257c9e1d953b
Adam Fabian <adam.fabian@rackspace.com>
2024-07-29 12:30:29 -0500

    WIP OSPC-551 add collecting timestamp metrics to ovn-backup.sh
    The Prometheus documentation suggested collecting the UNIX epoch timestamp to
    keep track of the amount of time since something happened. I didn't gather that
    as a metric since I believe every data point Prometheus collects explicitly has
    a timestamp. Perhaps we could use that to calculate the time since the last
    backup, but since the Prometheus documentation suggests simply collecting the
    timestamp as a metric, I will do that.
    
    I saw that documentation here:
    https://prometheus.io/docs/practices/instrumentation/#timestamps-not-time-since

--- base-kustomize/ovn/ovn-backup/ovn-backup.sh
+++ base-kustomize/ovn/ovn-backup/ovn-backup.sh
@@ -49,11 +49,17 @@ STATS_DIR="${BACKUP_DIR}/stats"
 
 declare -A metric_types=(
     ["run_count"]="counter"
+    ["run_timestamp"]="counter"
     ["save_pairs_to_disk_success_count"]="counter"
+    ["save_pairs_to_disk_success_timestamp"]="counter"
     ["save_pairs_to_disk_failure_count"]="counter"
+    ["save_pairs_to_disk_failure_timestamp"]="counter"
     ["upload_attempt_count"]="counter"
+    ["upload_attempt_timestamp"]="counter"
     ["upload_pairs_success_count"]="counter"
+    ["upload_pairs_success_timestamp"]="counter"
     ["upload_pairs_failure_count"]="counter"
+    ["upload_pairs_failure_timestamp"="counter"
     ["disk_files_gauge"]="gauge"
     ["swift_objects_gauge"]="gauge"
 )
@@ -96,7 +102,15 @@ increment() {
     update_metric "$METRIC_NAME" "$VALUE"
 }
 
+# Save epoch time to metric $1
+timestamp_metric() {
+    local METRIC_NAME
+    METRIC_NAME="$1"
+    update_metric "$METRIC_NAME" "$(date +%s)"
+}
+
 increment run_count
+timestamp_metric run_timestamp
 
 finalize_and_upload_metrics() {
     local FILE_COUNT
@@ -152,8 +166,10 @@ fi
 if [[ "$FAILED" == "true" ]]
 then
     increment save_pairs_to_disk_failure_count
+    timestamp_metric save_pairs_to_disk_failure_timestamp
 else
     increment save_pairs_to_disk_success_count
+    timestamp_metric save_pairs_to_disk_success_timestamp
 fi
 
 if [[ "$SWIFT_TEMPAUTH_UPLOAD" != "true" ]]
@@ -166,6 +182,7 @@ fi
 cd "$BACKUP_DIR" || exit 2
 
 increment upload_attempt_count
+timestamp_metric upload_attempt_timestamp
 
 # Make a working "swift" command
 SWIFT="kubectl -n openstack exec -i openstack-admin-client --
@@ -207,8 +224,10 @@ done
 if [[ "$FAILED_UPLOAD" == "true" ]]
 then
     increment upload_pairs_failure_count
+    timestamp_metric upload_pairs_failure_timestamp
 else
     increment upload_pairs_success_count
+    timestamp_metric upload_pairs_success_timestamp
 fi
 
 touch "$BACKUP_DIR/last_upload"
